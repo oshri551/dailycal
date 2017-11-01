bcd48837febb515f84aeddbfa8fd8d0e30c38916
Oshri <oshri.ifrah@intel.com>
2017-11-01 11:17:07 +0200

    angular fire

--- DailyMailUI/src/app/app.component.ts
+++ DailyMailUI/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessagingService } from './services/messaging.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  message;
+
+  constructor(private msgService: MessagingService) {
+    this.msgService.getPermission();
+    this.msgService.receiveMessage();
+    this.message = this.msgService.currentMessage;
+  }
 }
--- DailyMailUI/src/app/app.module.ts
+++ DailyMailUI/src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MessagingService } from './services/messaging.service';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // AngularFireModule.initializeApp({
+    //   apiKey: 'AIzaSyCLKqu5ESdtXK5_BbyqMJMCUo7ZGJjFUq8'
+    // })
+    // AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [MessagingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
