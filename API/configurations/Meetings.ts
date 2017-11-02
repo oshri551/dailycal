585c8d4d0707746be2e4722d070bb1bfb83c4137
Dan <dan.shmul@intel.com>
2017-11-02 09:19:03 +0200

    ww

--- API/configurations/Meetings.ts
+++ API/configurations/Meetings.ts
@@ -6,14 +6,14 @@ export const MEETINGS =
                                   phoneNumber : '086122663',
                                   location : 'IDC 9 - LTR Dotan & Merom'
     },
-    
+
     {
                                   startTime : 'Thu Nov 02 2017 10:00:00 GMT+0200',
                                   endTime : 'Thu Nov 02 2017 12:00:00 GMT+0200',
                                   phoneNumber : '086122663',
                                   location : 'IDC 11 - 105'
     },
-    
+
     {
                                   startTime : 'Thu Nov 02 2017 10:30:00 GMT+0200',
                                   endTime : 'Thu Nov 02 2017 11:30:00 GMT+0200',
@@ -26,14 +26,14 @@ export const MEETINGS =
                                   phoneNumber : '086122663',
                                   location : 'IDC 11 - '
     },
-    
+
     {
                                   m_startTime : 'Fri Nov 03 2017 20:00:00 GMT+0200',
                                   m_endTime : 'Fri Nov 03 2017 21:00:00 GMT+0200',
                                   phoneNumber : '086122663',
                                   m_location : 'TBD'
     },
-    
+
     {
                                   m_startTime : 'Fri Nov 03 01 2017 21:30:00 GMT+0200',
                                   m_endTime : 'Fri Nov 03 2017 22:30:00 GMT+0200',
